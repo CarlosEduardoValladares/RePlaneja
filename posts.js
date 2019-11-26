c2591568f8bb069814dd99d5ed18a06789eb6650
CarlosEduardoValladares <carloseduardovdm@gmail.com>
2019-11-26 16:30:41 -0300

    Feita a implementação do Cadastro

--- posts.js
+++ posts.js
@@ -1,6 +1,6 @@
-var postsList = JSON.parse('{"posts":[{"autor":"PessoaX","titulo":"Postagem 1","descricao":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo tincidunt ex. Proin sollicitudin velit quis dictum efficitur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam vestibulum neque et nulla scelerisque, et condimentum mauris iaculis. Sed velit urna, aliquet id laoreet volutpat, interdum ut enim. Donec ornare eget justo at sodales. In tempus eleifend nulla non efficitur. Duis consequat tortor lacus, eu hendrerit orci egestas nec. Aliquam vel quam dapibus, eleifend leo ut, fringilla tellus. Praesent mollis ipsum convallis, posuere nisl et, suscipit orci. Mauris id nulla id magna lobortis ornare. Nunc aliquam mollis risus, tempus efficitur eros consequat id. ","materia":"foo","serie":"bar"},{"autor":"PessoaY","titulo":"Postagem 2","descricao":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacinia libero id egestas fringilla. Proin placerat accumsan ante, ultricies varius nibh mollis at. Duis blandit placerat tincidunt. Sed dapibus eget neque non luctus. Quisque porttitor lobortis urna at tempus. Curabitur feugiat metus et commodo consectetur. Pellentesque tempor lacus lacus, bibendum ultricies nisl sodales quis. Mauris tempor, enim ut placerat mattis, nibh ante pulvinar diam, id tempus mauris quam vitae ex. Duis fringilla tincidunt sapien eget convallis. Cras a ex a dolor efficitur convallis in at nibh. Interdum et malesuada fames ac ante ipsum primis in faucibus. ","materia":"foo","serie":"bar"},{"autor":"PessoaZ","titulo":"Postagem 3","descricao":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu dapibus magna, a posuere nunc. Vestibulum aliquet vel erat vel mattis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Curabitur sed dictum turpis. Vivamus vehicula lectus et lacus laoreet, vel sagittis urna volutpat. Suspendisse rutrum semper consequat. Aliquam turpis nisi, eleifend eleifend malesuada sagittis, tristique quis eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras lacinia nisl id purus accumsan accumsan vitae in urna. Donec vehicula dolor ante, a posuere quam porttitor in. Curabitur ut lectus posuere, pharetra lectus eu, volutpat arcu. Vestibulum sed gravida nunc, sit amet fermentum mauris. Nam bibendum turpis eu lectus varius viverra et at felis. ","materia":"foo","serie":"bar"}]}');
+localStorage.setItem("posts", '{"posts":[{"autor":"PessoaX","titulo":"Postagem 1","descricao":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo tincidunt ex. Proin sollicitudin velit quis dictum efficitur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam vestibulum neque et nulla scelerisque, et condimentum mauris iaculis. Sed velit urna, aliquet id laoreet volutpat, interdum ut enim. Donec ornare eget justo at sodales. In tempus eleifend nulla non efficitur. Duis consequat tortor lacus, eu hendrerit orci egestas nec. Aliquam vel quam dapibus, eleifend leo ut, fringilla tellus. Praesent mollis ipsum convallis, posuere nisl et, suscipit orci. Mauris id nulla id magna lobortis ornare. Nunc aliquam mollis risus, tempus efficitur eros consequat id. ","materia":"foo","serie":"bar"},{"autor":"PessoaY","titulo":"Postagem 2","descricao":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacinia libero id egestas fringilla. Proin placerat accumsan ante, ultricies varius nibh mollis at. Duis blandit placerat tincidunt. Sed dapibus eget neque non luctus. Quisque porttitor lobortis urna at tempus. Curabitur feugiat metus et commodo consectetur. Pellentesque tempor lacus lacus, bibendum ultricies nisl sodales quis. Mauris tempor, enim ut placerat mattis, nibh ante pulvinar diam, id tempus mauris quam vitae ex. Duis fringilla tincidunt sapien eget convallis. Cras a ex a dolor efficitur convallis in at nibh. Interdum et malesuada fames ac ante ipsum primis in faucibus. ","materia":"foo","serie":"bar"},{"autor":"PessoaZ","titulo":"Postagem 3","descricao":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu dapibus magna, a posuere nunc. Vestibulum aliquet vel erat vel mattis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Curabitur sed dictum turpis. Vivamus vehicula lectus et lacus laoreet, vel sagittis urna volutpat. Suspendisse rutrum semper consequat. Aliquam turpis nisi, eleifend eleifend malesuada sagittis, tristique quis eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras lacinia nisl id purus accumsan accumsan vitae in urna. Donec vehicula dolor ante, a posuere quam porttitor in. Curabitur ut lectus posuere, pharetra lectus eu, volutpat arcu. Vestibulum sed gravida nunc, sit amet fermentum mauris. Nam bibendum turpis eu lectus varius viverra et at felis. ","materia":"foo","serie":"bar"}]}');
 
-/* JSON Legível:
+/* JSON legível dos posts:
 
 {
 
@@ -48,10 +48,6 @@ var allBeforeDelete = "');\">Denunciar &#9888;</button>	<button class=\"btn btn-
 var allBeforeDescription = "');\">Excluir &#128465;</button>		</div>	</div>	<div class=\"row ml-2 mr-2 mt-2\">		<div>			<p>Descrição: ";
 var stringEnd = "</p> </div> </div> </div>";
 
-
-
-
-
 /* Modelo de Postagem:
 
 <div class="border mt-4 postagem">
@@ -81,5 +77,34 @@ var stringEnd = "</p> </div> </div> </div>";
 		</div>
 	</div>
 </div>
+
+*/
+
+localStorage.setItem("cadastros", '{"cadastros":[{"nome": "Professor Padrão da Silva","email": "profpadrão@example.com","senha": "senhasegura123","confirmacao": "senhasegura123","area": "lpt","formacao": "grd"},{"nome": "Professora Registrada dos Santos","email": "profaregistrada@example.com","senha": "senhaboa1980","confirmacao": "senhaboa1980","area": "mat","formacao": "pdg"}]}');
+
+/* JSON legível dos cadastros
+
+{"cadastros":[
+
+        {
+          "nome": "Professor Padrão da Silva",
+          "email": "profpadrão@example.com",
+          "senha": "senhasegura123",
+          "confirmacao": "senhasegura123",
+          "area": "lpt",
+          "formacao": "grd"
+        },
+        {
+          "nome": "Professora Registrada dos Santos",
+          "email": "profaregistrada@example.com",
+          "senha": "senhaboa1980",
+          "confirmacao": "senhaboa1980",
+          "area": "mat",
+          "formacao": "pdg"
+        }
+
+
+    ]
+}
 
 */
