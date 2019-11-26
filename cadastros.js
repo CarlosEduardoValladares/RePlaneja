2a877b852c14e166a68a2e07ebd0baab99491ba1
CarlosEduardoValladares <carloseduardovdm@gmail.com>
2019-11-26 17:05:03 -0300

    Feita implementação parcial de login

---
+++ cadastros.js
@@ -0,0 +1,30 @@
+localStorage.setItem("cadastros", '{"cadastros":[{"nome": "Professor Padrão da Silva","email": "profpadrao@example.com","senha": "senhasegura123","confirmacao": "senhasegura123","area": "lpt","formacao": "grd"},{"nome": "Professora Registrada dos Santos","email": "profaregistrada@example.com","senha": "senhaboa1980","confirmacao": "senhaboa1980","area": "mat","formacao": "pdg"}]}');
+
+/* JSON legível dos cadastros
+
+{"cadastros":[
+
+        {
+          "nome": "Professor Padrão da Silva",
+          "email": "profpadrao@example.com",
+          "senha": "senhasegura123",
+          "confirmacao": "senhasegura123",
+          "area": "lpt",
+          "formacao": "grd"
+        },
+        {
+          "nome": "Professora Registrada dos Santos",
+          "email": "profaregistrada@example.com",
+          "senha": "senhaboa1980",
+          "confirmacao": "senhaboa1980",
+          "area": "mat",
+          "formacao": "pdg"
+        }
+
+
+    ]
+}
+
+*/
+
+localStorage.setItem("login", [0, null]);
--- posts.js
+++ posts.js
@@ -79,32 +79,3 @@ var stringEnd = "</p> </div> </div> </div>";
 </div>
 
 */
-
-localStorage.setItem("cadastros", '{"cadastros":[{"nome": "Professor Padrão da Silva","email": "profpadrão@example.com","senha": "senhasegura123","confirmacao": "senhasegura123","area": "lpt","formacao": "grd"},{"nome": "Professora Registrada dos Santos","email": "profaregistrada@example.com","senha": "senhaboa1980","confirmacao": "senhaboa1980","area": "mat","formacao": "pdg"}]}');
-
-/* JSON legível dos cadastros
-
-{"cadastros":[
-
-        {
-          "nome": "Professor Padrão da Silva",
-          "email": "profpadrão@example.com",
-          "senha": "senhasegura123",
-          "confirmacao": "senhasegura123",
-          "area": "lpt",
-          "formacao": "grd"
-        },
-        {
-          "nome": "Professora Registrada dos Santos",
-          "email": "profaregistrada@example.com",
-          "senha": "senhaboa1980",
-          "confirmacao": "senhaboa1980",
-          "area": "mat",
-          "formacao": "pdg"
-        }
-
-
-    ]
-}
-
-*/
