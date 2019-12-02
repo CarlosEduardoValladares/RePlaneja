7ed4838037e88e9f701c33a3dab3890a59d2ce0d
CarlosEduardoValladares <carloseduardovdm@gmail.com>
2019-12-02 11:17:38 -0300

    Colocado cadastro de admin e instrução de instalação

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Trabalho de Organização da Informação - 2019.2
 Sistema de repositório desenvolvido para que professores possam compartilhar planos de aula como alternativa aos métodos padrões de ensino
 
 [Documentação](https://github.com/CarlosEduardoValladares/RePlaneja/blob/master/Documenta%C3%A7%C3%A3o.docx)
+
+Para instalação (inicialização e eventual reinicialização do sistema), abra o arquivo initialize.html no navegador
--- cadastros.js
+++ cadastros.js
@@ -1,4 +1,4 @@
-localStorage.setItem("cadastros", '{"cadastros":[{"nome": "Professor Padrão da Silva","email": "profpadrao@example.com","senha": "senhasegura123","confirmacao": "senhasegura123","area": "lpt","formacao": "grd"},{"nome": "Professora Registrada dos Santos","email": "profaregistrada@example.com","senha": "senhaboa1980","confirmacao": "senhaboa1980","area": "mat","formacao": "pdg"}]}');
+localStorage.setItem("cadastros", '{"cadastros":[        {          "nome": "Professor Padrão da Silva",          "email": "profpadrao@example.com",          "senha": "senhasegura123",          "confirmacao": "senhasegura123",          "area": "lpt",          "formacao": "grd"        },        {          "nome": "Professora Registrada dos Santos",          "email": "profaregistrada@example.com",          "senha": "senhaboa1980",          "confirmacao": "senhaboa1980",          "area": "mat",          "formacao": "pdg"        },        {          "nome": "Administrador Alpha",          "email": "admin@example.com",          "senha": "admin",          "confirmacao": "admin"        }    ]}');
 
 /* JSON legível dos cadastros
 
@@ -19,6 +19,12 @@ localStorage.setItem("cadastros", '{"cadastros":[{"nome": "Professor Padrão da 
           "confirmacao": "senhaboa1980",
           "area": "mat",
           "formacao": "pdg"
+        },
+        {
+          "nome": "Administrador Alpha",
+          "email": "admin@example.com",
+          "senha": "admin",
+          "confirmacao": "admin"
         }
 
 
